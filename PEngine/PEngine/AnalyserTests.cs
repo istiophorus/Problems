558fc7db5f1fdd919826a1afd2698352dd7a591a
istiophorus <rskotak@gmail.com>
2017-03-05 01:16:30 +0100

    .

--- PEngine/PEngine/AnalyserTests.cs
+++ PEngine/PEngine/AnalyserTests.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PEngine
 {
@@ -73,6 +68,96 @@ namespace PEngine
                 });
 
             Assert.AreEqual(CardsAnalyser.HandRank.Pair, result.Rank);
+        }
+
+        [TestMethod]
+        public void TestStraight2()
+        {
+            CardsAnalyser.Result result = CardsAnalyser.Analyse(new[]
+                {
+                    new Card
+                    {
+                        Suit = CardSuit.Clubs,
+                        Symbol = CardSymbol.Jack
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.Ace
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Clubs,
+                        Symbol = CardSymbol.Three
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.King
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.Queen
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.Ten
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Spades,
+                        Symbol = CardSymbol.Jack
+                    }
+                });
+
+            Assert.AreEqual(CardsAnalyser.HandRank.Straight, result.Rank);
+        }
+
+        [TestMethod]
+        public void TestStraightVsThree()
+        {
+            CardsAnalyser.Result result = CardsAnalyser.Analyse(new[]
+                {
+                    new Card
+                    {
+                        Suit = CardSuit.Clubs,
+                        Symbol = CardSymbol.Jack
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.Ace
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Diamond,
+                        Symbol = CardSymbol.Jack
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.King
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.Queen
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Heart,
+                        Symbol = CardSymbol.Ten
+                    },
+                    new Card
+                    {
+                        Suit = CardSuit.Spades,
+                        Symbol = CardSymbol.Jack
+                    }
+                });
+
+            Assert.AreEqual(CardsAnalyser.HandRank.Straight, result.Rank);
         }
     }
 }
